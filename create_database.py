134d52d87a40201f638c01a9ce78c887ef3eedd0
Akshyan <Akshyan@bitbucket.com>
2018-07-07 17:16:36 +0530

    most of the functions for CRUD

--- create_database.py
+++ create_database.py
@@ -32,7 +32,7 @@ def insertworkerTable(con,x):
 		return "Not possible to isert"
 	with con:
 		cur= con.cursor()
-		cur.execute("INSERT INTO worker VALUES({},{},{},{},{},{},{},{},{},{},{},{},{})".format(*x))
+		cur.execute("INSERT INTO worker VALUES({},{},{},{},{},{},{},{},{},{},{},{},{})".format(tuple(x)))
 		return "Successful"
 def retrieveSchoolTable(con,SchoolId):
 	if SchoolId == '':
@@ -50,3 +50,89 @@ def retirveDistrict(con,district):
 		cur.execute("SELECT * FROM worker WHERE district={} ".format(district))
 		rows = cur.fetchall()
 		return rows
+def retirveBlock(con,block):
+        if block == '':
+                return "please select a block"
+        with con:
+                cur=con.cursor(mdb.cursors.DictCursor)
+                cur.execute("SELECT * FROM worker WHERE block={} ".format(block))
+                rows = cur.fetchall()
+                return rows
+def retrieveRegion(con,region):
+	if not region:
+		return "please select a region"
+	with con:
+		cur = con.cursor(mdb.cursors.DictCursor)
+		cur.execute("SELECT * FROM worker WHERE region={}".format(region))	
+		rows = cur.fetchall()
+		return rows
+def createFacilitiesTable(con):
+	with con:
+		cur = con.cursor()
+		cur.execute("DROP TABLE IF EXISTS Facilities")
+		cur.execute("CREATE TABLE Facilties(SchoolID INT PRIMARY KEY, Sports_facilities INT, Construction INT Child_fac INT, Meals INT )")
+	
+def InsertFacilitiesTable(con,a):
+	for i in a:
+		if  i < 0  or  i > 10:
+			return "Invalid Input"
+	with con:
+		cur = con.cursor()
+		cur.execute("INSERT INTO Facilities VALUES({},{},{},{},{})".format(tuple(a)))
+		return "successful" 
+
+def retriveFacilitiesTable(con,SchoolID):
+	if not SchoolID:
+		return "please enter the SchoolId"
+	with con:
+		cur = con.cursor(mdb.cursors.DictCursor)
+		cur.execute("SELECT * FROM Facilities WHERE SchoolID={}".format(SchoolID))
+		rows = cur.fetchall()
+		return rows
+def createAcademicsTable(con):
+        with con:
+                cur = con.cursor()
+                cur.execute("DROP TABLE IF EXISTS Facilities")
+                cur.execute("CREATE TABLE Facilties(SchoolID INT PRIMARY KEY, No_Academics_Progs INT, Pass_Percent INT, Comments VARCHAR(200))")
+        
+def InsertAcademicsTable(con,a):
+        for i in a:
+                if i < 0  or  i > 10:
+                        return "Invalid Input"
+        with con:
+                cur = con.cursor()
+                cur.execute("INSERT INTO Facilities VALUES({},{},{},{})".format(tuple(a)))
+                return "successful" 
+
+def retriveAcademicsTable(con,SchoolID):
+        if not SchoolID:
+                return "please enter the SchoolId"
+        with con:
+                cur = con.cursor(mdb.cursors.DictCursor)
+                cur.execute("SELECT * FROM Facilities WHERE SchoolID={}".format(SchoolID))
+                rows = cur.fetchall()
+                return rows
+def createSurveyTable(con):
+        with con:
+                cur = con.cursor()
+                cur.execute("DROP TABLE IF EXISTS Facilities")
+                cur.execute("CREATE TABLE Facilties(SchoolID INT PRIMARY KEY, Active_projects INT, Status INT,No_of_Months INT)")
+        
+def InsertSurveyTable(con,a):
+        for i in a:
+                if i < 0  or  i > 10:
+                        return "Invalid Input"
+        with con:
+                cur = con.cursor()
+                cur.execute("INSERT INTO Facilities VALUES({},{},{},{})".format(tuple(a)))
+                return "successful" 
+
+def retriveSurveyTable(con,SchoolID):
+        if not SchoolID:
+                return "please enter the SchoolId"
+        with con:
+                cur = con.cursor()
+                cur.execute("SELECT * FROM Facilities WHERE SchoolID={}".format(SchoolID))
+                rows = cur.fetchall()
+                return rows
+
