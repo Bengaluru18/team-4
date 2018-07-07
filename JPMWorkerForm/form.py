3da5a3e4fac265c548be7d71acc837225ba70817
Akshyan <Akshyan@bitbucket.com>
2018-07-07 17:27:58 +0530

    frontend

---
+++ JPMWorkerForm/form.py
@@ -0,0 +1,9 @@
+from flask import Flask, redirect, render_template, request, url_for, session
+app = Flask(__name__)
+@app.route("/", methods=['GET', 'POST'])
+def form():
+    if request.method=="GET":
+        return render_template("index.html")
+
+if __name__ == '__main__':
+	app.run()
