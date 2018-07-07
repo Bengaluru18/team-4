ac3a6698e4eee16f0c6f774c7fed661cbb81b6b6
AmoghVardhan <kashiamoghvardhan@gmail.com>
2018-07-07 17:36:50 +0530

    basic structure + aws jar files

---
+++ cfg/app/src/main/java/com/example/ksav/cfg/HealthActivity.java
@@ -0,0 +1,13 @@
+package com.example.ksav.cfg;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HealthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_health);
+    }
+}
