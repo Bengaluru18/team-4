58a5549924c4ca4104910fb49908b7dda5dbe592
MahadevMohitNK <35162375+MahadevMohit@users.noreply.github.com>
2018-07-08 10:41:11 +0530

    Final version

--- JPMLogin/login.py
+++ JPMLogin/login.py
@@ -4,6 +4,15 @@ app = Flask(__name__)
 def login():
     if request.method=="GET":
         return render_template("index.html")
+@app.route("/dash", methods=['GET', 'POST'])
+def dash():
+    if request.method=="GET":
+        return render_template("dash.html")
+@app.route("/report", methods=['GET', 'POST'])
+def report():
+    if request.method=="GET":
+        return render_template("report.html")
+
 
 if __name__ == '__main__':
 	app.run()
