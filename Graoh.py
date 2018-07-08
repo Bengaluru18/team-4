332e15e448f78b932728d3d815543f08f071c746
vashok890 <37514073+vashok890@users.noreply.github.com>
2018-07-08 09:16:15 +0530

    Graphs code

---
+++ Graoh.py
@@ -0,0 +1,8 @@
+#Graph
+print max1
+p1 = plt.bar(y, z, w)
+plt.ylabel('Score values')
+plt.title('')
+plt.xticks(y, ('school current', 'max value', 'average value'))
+plt.savefig("D:/plot.png")
+plt.show()
